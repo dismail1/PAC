7356496e2a7eb899ba431d1a0df3c9bed509a3c6
daneih ismail <dismail1@luc.edu>
2014-02-19 12:22:04 -0600

    activity classes with labels

--- PAC.php
+++ PAC.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Loyola PAC Lab</title>
-	<link rel="stylesheet" href="PAC_v1.css">
+	<link rel="stylesheet" href="PAC.css">
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"> </script>
   <script language="javascript" type="text/javascript" src="jquery.js"></script>
   <script language="javascript" type="text/javascript" src="jquery.flot.js"></script>
@@ -138,6 +138,11 @@ $(function () {
 
     <br>Activity Type
     <div id="placeholder2"></div>
+    <table>
+    <tr><td bgcolor="#FFD700">&nbsp;</td><td>Lying</td><td bgcolor="#87CEEB">&nbsp;</td><td>Wheeling</td><td bgcolor="#DC143C">&nbsp;</td><td>Walking</td></tr>
+<tr><td bgcolor="#228B22">&nbsp;</td><td>Sitting</td><td bgcolor="#9400D3">&nbsp;</td><td>Misc</td><td bgcolor="#B8860B">&nbsp;</td><td>Standing</td></tr>
+</table>
+
       <script type="text/javascript">
         // Data from JSON file is stored in an array
         var timeInSeconds = new Array();
@@ -155,12 +160,38 @@ $(function () {
           
           $(function () {
             var d1 = [];
+            var d2 = [];
+            var d3 = [];
+            var d4 = [];
+            var d5 = [];
+            var d6 = [];
 
             for (var t = 0; t <= amtData; t++) {
-              d1.push([timeInSeconds[t],activity[t]]);
+              if (activity[t]=="Lying" && activity[t+1]=="Lying")
+                d1.push([timeInSeconds[t],timeInSeconds[t]]);
+              else
+                d1.push(null);
+              if (activity[t]=="Wheeling" && activity[t+1]=="Wheeling")
+                d2.push([timeInSeconds[t],timeInSeconds[t]]);
+              else
+                d2.push(null);
+              if (activity[t]=="Walking" && activity[t+1]=="Walking")
+                d3.push([timeInSeconds[t],timeInSeconds[t]]);
+              else
+                d3.push(null);
+              if (activity[t]=="Sitting" && activity[t+1]=="Sitting")
+                d4.push([timeInSeconds[t],timeInSeconds[t+1]]);
+              else
+                d4.push(null);
+              if (activity[t]=="Misc" && activity[t+1]=="Misc")
+                d5.push([timeInSeconds[t],timeInSeconds[t+1]]);
+              else
+                d5.push(null);
+              if (activity[t]=="Standing" && activity[t+1]=="Standing")
+                d6.push([timeInSeconds[t],timeInSeconds[t+1]]);
+              else
+                d6.push(null);
             }
-
-            var xData = [d1];
   
             var placeholder2 = $("#placeholder2");
             var options = {
@@ -183,7 +214,7 @@ $(function () {
               }
             };
   
-            var plot = $.plot(placeholder2, [d1], options);
+            var plot = $.plot(placeholder2, [d1,d2,d3,d4,d5], options);
             placeholder2.bind('plotpan', function (event, plot) {
               var axes = plot.getAxes();
               $(".message").html("Panning to x: "  + axes.xaxis.min.toFixed(2)
