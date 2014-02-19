fd48ddd2f5fbf151b553ccca6accec6e86b648ae
daneih ismail <dismail1@luc.edu>
2014-02-18 21:22:10 -0600

    Get patient information

--- PAC_v1.php
+++ PAC_v1.php
@@ -13,25 +13,21 @@
 	<div id="left">
     <script type="text/javascript">
       // Get patient ID, type and in lab/at home
-      var patientID = "";
-      var patientType = "";
-      var inLabAtHome = "";
-      $.getJSON('PatientInfo.json', function(data) {
-        $.each(data.patientInfo, function(f, i) {
-          patientID = f.PatientID;
-          patientType = f.PatientType;
-          inLabAtHome = f.InLabAtHome;
-        });
+$(function () {
+
+  // Get patient ID, type and in lab/at home
+  var patientID = "";
+  var patientType = "";
+  var inLabAtHome = "";
+ $.getJSON('PatientInfo.json', function(data) {
+        document.getElementById("patient").innerHTML="Patient ID: "+data.PatientID+'<br>'+"Patient type: "+data.PatientType+'<br>'+"In lab/at home: "+data.InLabAtHome;
+            });
       });
-      $(patientID).appendTo("#patientID");
-      $(patientType).appendTo("#patientType");
-      $(inLabAtHome).appendTo("#inLabAtHome");
     </script>
 
 		<p id="LoyolaPACLab">Loyola PAC Lab<br>
-		<span id="patient">Patient ID: </span><span id="patientID"></span><br>
-		<span id="patient">Patient type: </span><span id="patientType"><br>
-		<span id="patient">In lab/at home: </span><span id="inLabAtHome"></p>
+<div id="patient"></div><br>
+</p>
 
 		<p>Comments:
 		<textarea rows="10" cols="45"></textarea></p>
