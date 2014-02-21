ee4c0d160082490dd5bdb8f10279023dbbbdde01
Carol Klann <cklann@luc.edu>
2014-02-21 17:05:20 -0600

    Edited Activity Type chart
    Edited Activity Type chart: Made line thicker, made y-axis equal to 1
    for all, rearranged colors slightly

--- PAC.php
+++ PAC.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Loyola PAC Lab</title>
-	<link rel="stylesheet" href="PAC_v1.css">
+  <title>Loyola PAC Lab</title>
+  <link rel="stylesheet" href="PAC.css">
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"> </script>
   <script language="javascript" type="text/javascript" src="jquery.js"></script>
   <script language="javascript" type="text/javascript" src="jquery.flot.js"></script>
@@ -10,35 +10,27 @@
 </head>
 
 <body>
-	<div id="left">
+  <div id="left">
     <script type="text/javascript">
       // Get patient ID, type and in lab/at home
-      var patientID = "";
-      var patientType = "";
-      var inLabAtHome = "";
-      $.getJSON('PatientInfo.json', function(data) {
-        $.each(data.patientInfo, function(f, i) {
-          patientID = f.PatientID;
-          patientType = f.PatientType;
-          inLabAtHome = f.InLabAtHome;
+      $(function() {
+        $.getJSON('PatientInfo.json', function(data) {
+          document.getElementById("patient").innerHTML="Patient ID: "+data.PatientID+"<br>"+
+                                                       "Patient type: "+data.PatientType+"<br>"+
+                                                       "In lab/at home: "+data.InLabAtHome;
         });
       });
-      $(patientID).appendTo("#patientID");
-      $(patientType).appendTo("#patientType");
-      $(inLabAtHome).appendTo("#inLabAtHome");
     </script>
 
-		<p id="LoyolaPACLab">Loyola PAC Lab<br>
-		<span id="patient">Patient ID: </span><span id="patientID"></span><br>
-		<span id="patient">Patient type: </span><span id="patientType"><br>
-		<span id="patient">In lab/at home: </span><span id="inLabAtHome"></p>
+    <p id="LoyolaPACLab">Loyola PAC Lab<br>
+    <div id="patient"></div></p>
 
-		<p>Comments:
-		<textarea rows="10" cols="45"></textarea></p>
-	</div>
+    <p>Comments:
+    <textarea rows="10" cols="45"></textarea></p>
+  </div>
 
-	<div id="right">
-		<p><img src="luc-logo.png" alt="Loyola logo" align="right"></p><br><br><br><br><br><br>
+  <div id="right">
+    <p><img src="luc-logo.png" alt="Loyola logo" align="right"></p><br><br><br><br>
 
     Activity
     <div id="placeholder"></div>
@@ -144,6 +136,24 @@
 
     <br>Activity Type
     <div id="placeholder2"></div>
+      <table>
+        <tr>
+          <td id="cell"></td>
+          <td id="colorBox" bgcolor="#FFD700"></td>
+          <td id="cell">&nbsp;Lying</td>
+          <td id="colorBox" bgcolor="#87CEEB"></td>
+          <td id="cell">&nbsp;Wheeling</td>
+          <td id="colorBox" bgcolor="#DC143C"></td>
+          <td id="cell">&nbsp;Walking</td>
+          <td id="colorBox" bgcolor="#228B22"></td>
+          <td id="cell">&nbsp;Sitting</td>
+          <td id="colorBox" bgcolor="#B8860B"></td>
+          <td id="cell">&nbsp;Standing</td>
+          <td id="colorBox" bgcolor="#FFFFFF"></td>
+          <td id="cell">&nbsp;Misc.</td>
+        </tr>
+      </table>
+
       <script type="text/javascript">
         // Data from JSON file is stored in an array
         var timeInSeconds = new Array();
@@ -151,8 +161,6 @@
         var i = 0;
 
         $.getJSON('SampleData.json', function(data) {
-      
-          // Read data from SampleData.json
           $.each(data.patientData, function(i, f) {
             timeInSeconds[i] = f.TimeInSeconds;
             activity[i] = f.Activity;
@@ -161,19 +169,54 @@
           
           $(function () {
             var d1 = [];
+            var d2 = [];
+            var d3 = [];
+            var d4 = [];
+            var d5 = [];
+            var d6 = [];
 
             for (var t = 0; t <= amtData; t++) {
-              d1.push([timeInSeconds[t],activity[t]]);
-            }
+              if (activity[t]=="Lying")
+                d1.push([timeInSeconds[t], 1]);
+              else
+                d1.push(null);
 
-            var xData = [d1];
+              if (activity[t]=="Wheeling")
+                d2.push([timeInSeconds[t], 1]);
+              else
+                d2.push(null);
+
+              if (activity[t]=="Walking")
+                d3.push([timeInSeconds[t], 1]);
+              else
+                d3.push(null);
+
+              if (activity[t]=="Sitting")
+                d4.push([timeInSeconds[t], 1]);
+              else
+                d4.push(null);
+
+              if (activity[t]=="Standing")
+                d5.push([timeInSeconds[t], 1]);
+              else
+                d5.push(null);
+
+              if (activity[t]=="Misc")
+                d6.push([timeInSeconds[t], 1]);
+              else
+                d6.push(null);
+            }
   
             var placeholder2 = $("#placeholder2");
             var options = {
               series: {
-                lines: {show: true},
+                lines: {
+                  show: true,
+                  lineWidth: 10
+                },
                 shadowSize: 0
               },
+              colors: ["#FFD700", "#87CEEB", "#DC143C", "#228B22", "#B8860B", "#FFFFFF"],
               xaxis: {
                 zoomRange: [0.1, 1000000], panRange: [-1000000, 1000000]
               },
@@ -189,7 +232,7 @@
               }
             };
   
-            var plot = $.plot(placeholder2, [d1], options);
+            var plot = $.plot(placeholder2, [d1,d2,d3,d4,d5], options);
             placeholder2.bind('plotpan', function (event, plot) {
               var axes = plot.getAxes();
               $(".message").html("Panning to x: "  + axes.xaxis.min.toFixed(2)
@@ -229,7 +272,7 @@
         });
     </script>
 
-	</div>
+  </div>
 
 </body>
 </html>
